aad6c7ec0a229f00069752187bd06e05a70a32a2
AgustinM12 <agustinmazza12345@gmail.com>
2024-09-01 20:46:05 -0300

    fix: :bug: se arreglo el error con funciones de normalizacion de datos y se mejoraron los prints

--- main.py
+++ main.py
@@ -11,50 +11,47 @@ datos = pd.read_csv('altura_peso.csv', sep=",")
 x = datos["Altura"].values
 y = datos["Peso"].values
 
+# Normalizar los datos SOLUCIONA EL ERROR DE LOS NAN e INF
+x = (x - np.mean(x)) / np.std(x)
+y = (y - np.mean(y)) / np.std(y)
+
+print(x)
+print(y)
+
 # Indicar que los randoms sean siempre iguales
 np.random.seed(2)
 
 # Crear el modelo
 modelo = Sequential()
 
-# # Configuracion del modelo
-input_dim = 1
-output_dim = 1
-modelo.add(Dense(units=output_dim, input_dim=input_dim, activation='linear'))
+# Configuracion del modelo con inicialización HeNormal
+modelo.add(Dense(units=1, input_dim=1, activation='linear'))
 
-# # Stochastic Gradient Descent, indica que el método de entrenamiento será precisamente el del Gradiente Descendente.
+# Stochastic Gradient Descent con un learning rate más bajo
 sgd = SGD(learning_rate=0.0004)
 
-# # Agregar la funcion de perdida "error cuadratico medio" y el optimizador 
+# Compilar el modelo
 modelo.compile(loss='mse', optimizer=sgd)
 
-# # ver las caracteristicas del modelo
+# Ver las características del modelo
 modelo.summary()
 
-# # configurar las epocas, cantidad de datos que usara en el entrenamiento y el historial del entrenamiento 
-num_epochs = 10000
+# Configurar las épocas y el historial del entrenamiento
 batch_size = x.shape[0]
-historia = modelo.fit(x, y, epochs=num_epochs, batch_size=batch_size,
-verbose=1)
 
-# definir las capas que tendra el modelo
-# Accede a la primera capa
-primer_capa = modelo.layers[0]
-
-# Obtén los pesos y sesgos de la primera capa
-w, b = primer_capa.get_weights()
-print("Pesos (w):")
-print(w)
-
-print("Sesgos (b):")
-print(b)
+# Monitorizar el entrenamiento
+# aproximadamente en 6000 epocas se estanca la perdida 
+historia = modelo.fit(x, y, epochs=10000, batch_size=batch_size, verbose=1)
+w, b = modelo.layers[0].get_weights()
+print(f"Pesos (w) = {w[0][0]}, Sesgos (b) = {b[0]}")
 
 # probar el modelo de prediccion 
 x_pred = np.array([170])
 y_pred = modelo.predict(x_pred)
-print(f"El peso de una persona con una altura {x_pred} sera aproximadamente: {y_pred}")
+print(f"El peso de una persona con una altura {x_pred[0]} sera aproximadamente: {y_pred[0][0]}")
 
-# graficar los datos de perdida durante el entrenamiento del modeloplt.subplot(1,2,1)
+# Graficar los datos de pérdida durante el entrenamiento del modelo
+plt.subplot(1,2,1)
 plt.plot(historia.history['loss'])
 plt.xlabel('epoch')
 plt.ylabel('ECM')
