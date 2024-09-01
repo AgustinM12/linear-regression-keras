aaab031309bbcce61c75e9c995d4242940f6242d
AgustinM12 <agustinmazza12345@gmail.com>
2024-08-31 23:50:27 -0300

    feat:

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# from keras.api.models import Sequential
+# from keras.api.layers import Dense
+# from keras.api.optimizers import SGD
+
+# Cargar los datos deñ csv
+datos = pd.read_csv('altura_peso.csv')
+print(datos)
+
+
+
